795e29155c224457a22c4ccc435550bcdc78b345
Kolomeichenko <admirall.buldog@gmail.com>
2019-03-05 20:46:30 +0200

    4 урок

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// так пишется комментарий
+alert('Hello, world');
+console.log('Hello, console'); //коммент
+/* вот
+так
+комментим
+в нескольких строках
+ */
+var messageText;
+var count2 = 2; //можно тут значение влепить
+var message1='fff', label, message3='ddd'; //можно всё разом писать
+
+messageText = 'текст сообщения';
+count2=2; //можно значение влепить и тут
+
