6b06f653bf8c9d09dab7cbefc511ad6045f39125
Kolomeichenko <admirall.buldog@gmail.com>
2019-03-12 19:11:49 +0200

    5 урок

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
 "use strict";
 
-var num1, num2, result;
-
-num1 = +prompt('введите 1ое число', 0);
-num2 = +prompt('введите 2е число', 0);
-
-result = num1 + num2;
-
-alert('результат сложения: ' + result);
