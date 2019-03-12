500e05135b596aedaf30233cf264268b6401c763
Kolomeichenko <admirall.buldog@gmail.com>
2019-03-12 21:16:03 +0200

    5 урок fin

---
+++ task1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = prompt('каково официальное название JavaScript?');
+if (a === 'ECMAScript') {
+        alert('верно!');
+} else {
+        alert('не знаете? ECMAScript!');
+}
+
+
